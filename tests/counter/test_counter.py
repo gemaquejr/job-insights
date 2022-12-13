89459aefd898d2cb4af585d34d29b4482b1c9d0c
gemaquejr <gemaquejr@hotmail.com>
2022-12-13 16:09:04 -0300

    Adiciona a funcao count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
+# https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison
 def test_counter():
-    pass
+    assert count_ocurrences('data/jobs.csv', "Marketing".casefold()) == 1259
