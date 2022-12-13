2427b0afee49249159a425e2a31243898577e66a
gemaquejr <gemaquejr@hotmail.com>
2022-12-13 13:30:26 -0300

    Adiciona a funcao get_max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,16 @@
 from typing import Union, List, Dict
 
+from src.insights.jobs import read
 
-def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+# Ajuda da Maria Carolina na mentoria
+def get_max_salary(path):
+    salaries = read(path)
+    all_salaries = set()
+    for salary in salaries:
+        if salary["max_salary"].isnumeric():
+            all_salaries.add(int(salary["max_salary"]))
+    return max(all_salaries)
 
 
 def get_min_salary(path: str) -> int:
