f3d10b63830bcc11e43fd18937396b63a90b6e87
gemaquejr <gemaquejr@hotmail.com>
2022-12-13 20:40:41 -0300

    Adiciona o teste para a funcao sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,12 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 100, "max_salary": 200, "date_posted": "2022-12-11"},
+        {"min_salary": 200, "max_salary": 300, "date_posted": "2022-12-12"},
+        {"min_salary": 300, "max_salary": 400, "date_posted": "2022-12-13"},
+    ]
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == 400
