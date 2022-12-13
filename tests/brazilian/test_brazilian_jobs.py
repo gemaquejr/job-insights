70eb68530f74570e3788f357097ad9432e963397
gemaquejr <gemaquejr@hotmail.com>
2022-12-13 18:10:21 -0300

    Adiciona o teste para a funcao read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,6 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    translated = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert "title" in translated[0]
