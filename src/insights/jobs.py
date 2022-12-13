243997f0407da51c35d0cf1c9848f8f8e767ae46
gemaquejr <gemaquejr@hotmail.com>
2022-12-13 14:04:31 -0300

    Adiciona a funcao filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-from typing import List, Dict
 
 import csv
 
@@ -20,19 +19,9 @@ def get_unique_job_types(path):
     return all_jobs
 
 
-def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+def filter_by_job_type(jobs, job_type):
+    list_of_job_types = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            list_of_job_types.append(job)
+    return list_of_job_types
